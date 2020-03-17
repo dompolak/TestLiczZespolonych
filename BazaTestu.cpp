b34fa4aad040d51876409d7518e1f7365f685aa8
Dominik Polak <252923@student.pwr.edu.pl>
2020-03-17 20:19:57 +0100

    nie

--- BazaTestu.cpp
+++ BazaTestu.cpp
@@ -24,18 +24,18 @@ void inicjalizuj_test(const std::string typ_testu)
     {
         std::cerr << "Inicjalizacja testu nie powiodla sie!" <<std:: endl;
     }else
-    {   
+    {
         std::cout << "Start testu:" << std::endl;
         while(!file.eof())
-        {   
+        {
             file >> pytanie_testowe;
             if(file.eof()) break;
-            std::cout << "Podaj wynik operacji: " << pytanie_testowe << " = ";  
+            std::cout << "Podaj wynik operacji: " << pytanie_testowe << " = ";
             std::cin >> z1;
             std::cout << "Twoja odpowiedz: " << z1 << std::endl;
             sprawdz(z1, pytanie_testowe, stat);
         }
-        std::cout << "Ilosc dobrych odpowiedzi: " << stat.getPoprawne() << std::endl; 
+        std::cout << "Ilosc dobrych odpowiedzi: " << stat.getPoprawne() << std::endl;
         std::cout << "Ilosc blednych odpowiedzi: " << stat.getNiepoprawne() << std::endl;
         std::cout << "Procentowy wynik testu: " << stat.procent() << "%" << std::endl << std::endl;
         std::cout << "Koniec testu" << std::endl;
@@ -52,7 +52,7 @@ void sprawdz(const LZespolona &z1, Wyra_zespolone &pytanie_testowe, statystyka &
     stat.licz_pytania();
     wynik = pytanie_testowe.oblicz_wyrazenie(pytanie_testowe);
     if(wynik == z1)
-    {   
+    {
         stat.licz_poprawne();
         std::cout << "Odpowiedz poprawna" << std::endl;
     }else
@@ -89,7 +89,7 @@ int statystyka::getNiepoprawne()
 double statystyka::procent()
 {
     return (this->poprawne/this->ilosc_pytan) * 100;
-    
+
 }
 
 statystyka::statystyka()
@@ -102,3 +102,7 @@ statystyka::~statystyka()
 {
 
 }
+
+nie nie
+nie nie
+nie nie
